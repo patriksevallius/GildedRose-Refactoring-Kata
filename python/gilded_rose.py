0e4bf73e156b6c952d40f5d204a014bc2ac97d14
Patrik Sevallius <patrik.sevallius@assaabloy.com>
2018-07-04 21:01:28 +0200

    Made factory method out of Item.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,32 +1,54 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
 
+class GildedRose:
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name == "Aged Brie":
-                item.update_brie_quality()
-            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                item.update_backstage_quality()
-            elif item.name == "Sulfuras, Hand of Ragnaros":
-                item.update_sulfuras_quality()
-            else:
-                item.update_item_quality()
+            item.update_quality()
 
 
 class Item:
+    def __new__(cls, name, *args):
+        if name == "Aged Brie":
+            return super().__new__(BrieItem)
+        elif name == "Backstage passes to a TAFKAL80ETC concert":
+            return super().__new__(BackstageItem)
+        elif name == "Sulfuras, Hand of Ragnaros":
+            return super().__new__(SulfurasItem)
+        else:
+            return super().__new__(cls)
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
 
-    def update_sulfuras_quality(self):
-        pass
+    def update_quality(self):
+        self.sell_in -= 1
+        if self.sell_in < 0:
+            if self.quality > 0:
+                self.quality -= 1
+        if self.quality > 0:
+            self.quality -= 1
 
-    def update_backstage_quality(self):
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class BrieItem(Item):
+    def update_quality(self):
+        self.sell_in -= 1
+        if self.quality < 50:
+            self.quality += 1
+        if self.sell_in < 0 and self.quality < 50:
+            self.quality += 1
+
+
+class BackstageItem(Item):
+    def update_quality(self):
         self.sell_in -= 1
         if self.sell_in < 0:
             self.quality = 0
@@ -38,20 +60,7 @@ class Item:
             if self.sell_in < 5 and self.quality < 50:
                 self.quality += 1
 
-    def update_brie_quality(self):
-        self.sell_in -= 1
-        if self.quality < 50:
-            self.quality += 1
-        if self.sell_in < 0 and self.quality < 50:
-            self.quality += 1
 
-    def update_item_quality(self):
-        self.sell_in -= 1
-        if self.sell_in < 0:
-            if self.quality > 0:
-                self.quality -= 1
-        if self.quality > 0:
-            self.quality -= 1
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+class SulfurasItem(Item):
+    def update_quality(self):
+        pass
